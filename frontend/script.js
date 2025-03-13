e68394101675a4b6224f7a80fbb167b0d6ee69b1
Kenrick14 <browntajaybt@gmail.com>
2025-03-13 17:51:21 -0500

    Created navbar, improved flashcard modal, initiated home page layout -created navbar -refactored code for creating flashcard modal -started home page
    Co-authored-by: Leon <noel12sirrom@users.noreply.github.com>

--- frontend/script.js
+++ frontend/script.js
@@ -2,22 +2,38 @@ const cardButton = document.getElementById("save-btn");
 const question = document.getElementById("question");
 const answer = document.getElementById("answer");
 const errorMessage = document.getElementById("error");
+const savedMessage = document.getElementById("saved");
 const closeButton = document.getElementById("close-btn");
+const newCard = document.getElementById("newcard-btn");
+const flashcard = document.getElementById("modal");
 
 closeButton.addEventListener("click", (hideQuestion = () => {
+    flashcard.classList.remove("flex");
+    flashcard.classList.add("hidden");
+    question.value = "";
+    answer.value = "";
+    errorMessage.classList.add("hidden");
+}))
 
+newCard.addEventListener("click", (showFlashCard = () => {
+    flashcard.classList.remove("hidden");
+    flashcard.classList.add("flex");
+    question.value = "";
+    answer.value = "";
+    errorMessage.classList.add("hidden");
 }))
 
 cardButton.addEventListener("click", (saveCard = () => {
     tempQuestion = question.value.trim();
     tempAnswer = answer.value.trim();
     if (!tempQuestion || !tempAnswer) {
-        errorMessage.classList.remove("invisible");
+        errorMessage.classList.remove("hidden");
         return;
     }
     else {
         makePost();
-        errorMessage.classList.add("invisible");
+        savedMessage.classList.remove("hidden");
+        errorMessage.classList.add("hidden");
         question.value = "";
         answer.value = "";
 
